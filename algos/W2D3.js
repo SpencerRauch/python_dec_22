77aa11b407161ee6086cf150d83ce65449e31cef
Spencer Rauch <rauchs@gmail.com>
2022-12-07 09:03:56 -0800

    solutions

--- algos/W2D3.js
+++ algos/W2D3.js
@@ -62,3 +62,87 @@ function bracesValid(str) {
 console.log(bracesValid(strA)) // expected: true
 console.log(bracesValid(strB)) // expected: false
 console.log(bracesValid(strC)) // expected: false
+
+
+
+
+function parensValid(str) {
+    let unmatchedOpens = 0;
+    for (let char of str) {
+        if (char === "(") unmatchedOpens++
+        if (char === ")") unmatchedOpens--
+        if (unmatchedOpens < 0) return false
+    }
+    return unmatchedOpens == 0;
+}
+
+
+
+
+
+
+function bracesValid(str) {
+    const stack = [];
+    const isOpen = { "(": true, "{": true, "[": true }
+    const closesToOpens = { ")": "(", "}": "{", "]": "[" };
+
+    for (let char of str) {
+        if (isOpen[char]) {
+            stack.push(char);
+        } else if (closesToOpens[char]) {
+            if (closesToOpens[char] === stack[stack.length - 1]) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+}
+
+
+
+
+
+//from Aarti Chandrasekar
+
+const parenMap = new Map([["{", "}"],
+["(", ")"],
+["[", "]"]]);
+
+function bracesValid(str) {
+    // const strB = "D(i{a}l[ t]o)n{e";
+    var len = str.length;
+    var parenList = [];
+    // For each character in the string, check if it is one of the valid OPEN parenthesis
+    // and add the OPEN parenthesis to a list. When we encounter a CLOSE parenthesis, 
+    // pop the last OPEN parenthesis from the parenthesis List.
+    // Check if it is the CLOSE parenthesis we are expecting for the OPEN parenthesis 
+    // if ( then we are expecting ), { then we are expecting }, and so on
+    // if it is not what is expected return false
+    for (var i = 0; i < len; i++) {
+        // console.log(parenList);
+        if (parenMap.has(str[i])) { // if a valid OPEN parenthesis
+            parenList.push(str[i]);
+        } else if (contains(parenMap.values(), str[i])) { // if a valid CLOSE parenthesis
+            // if not a matching closing bracket
+            // For example, if the last 
+            if (parenMap.get(parenList.pop()) != str[i]) {
+                return false;
+            }
+        }
+    }
+    if (parenList.length > 0) {
+        return false;
+    }
+    return true;
+}
+
+function contains(itr, letter) {
+    for (var val of itr) {
+        if (val == letter) {
+            return true;
+        }
+    }
+    return false;
+}
