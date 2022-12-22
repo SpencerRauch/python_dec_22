ac245e36ff4d61fb6fe362e08a803f685585e444
Spencer Rauch <rauchs@gmail.com>
2022-12-22 09:00:53 -0800

    solutions

--- algos/W4D4.js
+++ algos/W4D4.js
@@ -42,3 +42,102 @@ console.log(binaryStringExpansion(str1)) // expected ["1000", "1001", "1100", "1
 console.log(binaryStringExpansion(str2)) // expected ['000', '001', '010', '011', '100', '101','110', '111']
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function binaryStringExpansion(str, solutions = [], processed = "") {
+    const index = str.indexOf("?"); // will return index of first "?" or -1
+
+    if (index < 0) { //if we don't find a "?" we're done
+        solutions.push(processed + str);
+    } else {
+        const front = str.slice(0, index); //take everything before the ?
+        const back = str.slice(index + 1); // take everything after the ?
+        const prefix = processed + front; //moved the front into the already processed string
+        binaryStringExpansion(back, solutions, prefix + "0"); //pass the back as what's left to process, add 0 to processed string
+        binaryStringExpansion(back, solutions, prefix + "1"); // same but adding 1 to processed string
+    }
+    return solutions;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// No built-ins version
+function binaryStringExpansion(str, solutions = [], processed = "") {
+    //Your code here
+    // base case? 
+    let found = false; //searching for "?"
+    let index = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === "?") { // if found,   
+            index = i; //capture index,
+            found = true; //set found to true,
+            break //and stop loop
+        }
+    }
+    if (!found) { //-- if no ? left, done with recursion
+        solutions.push(processed + str) //put the string back together and add to solutions
+        return // end function call
+    }
+    // logic? 
+
+    for (let i = 0; i < index; i++) { // grab all the chars before the ?
+        processed += str[i] // they are now considered processed
+    }
+
+    let toBeProcessed = ""; // grab all the chars after the ?
+    for (let i = index+1; i < str.length; i++) {
+        toBeProcessed += str[i] // they are now what's left to process
+    }
+    // recursive call?
+    binaryStringExpansion(toBeProcessed, solutions, processed + "0") // one call adding 0 for the ?
+    binaryStringExpansion(toBeProcessed, solutions, processed + "1") // one call adding 1 for the ?
+    // return?
+    return solutions
+}
+
+function binaryStringExpansion(str, results = []) {
+    var i = str.indexOf("?")
+    if (i == -1){
+        results.push(str);
+    }
+    else{
+        var strArr = str.split("");
+        strArr[i] = 0;
+        binaryStringExpansion(strArr.join(""), results);
+        strArr[i] = 1;
+        binaryStringExpansion(strArr.join(""), results);
+    }
+
+    return results;
+}
+
+
+
+
+
+
